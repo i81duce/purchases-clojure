b1a5f1a0e954d183088afd28defb5cf1baf58410
KevinBozic <bozic.kevin@gmail.com>
2016-03-22 16:08:50 -0400

    Finished assginment

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -2,18 +2,26 @@
   (:require [clojure.string :as str]
             [clojure.walk :as walk])
   (:gen-class))
-
+; Furniture, Alcohol, Toiletries, Shoes, Food, Jewelry
 (defn -main []
+  (println "Please enter a category: Furniture - Alcohol - Toiletries - Shoes - Food - Jewelry")
   (let [purchases (slurp "purchases.csv")
         purchases (str/split-lines purchases)
         purchases (map (fn [line]
                          (str/split line #","))
-                    purchases)
+                   purchases)
         header (first purchases)
-        purchases (rest purchases)]
+        purchases (rest purchases)
+        purchases (map (fn [line]
+                         (apply hash-map (interleave header line)))
+                   purchases)
+        purchases (walk/keywordize-keys purchases) 
+        text (read-line)
+        purchases (filter (fn [line]
+                            (= text (:category line)))
+                   purchases)]
+    (spit "filtered_purchases.edn" (pr-str purchases))
     purchases))
-       
+    
 
-        
-
-        
+         
